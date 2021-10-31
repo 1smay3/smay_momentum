afd35080bcd731353e295da7821770ff2e7f2d9f
1smay3 <spencermayy@gmail.com>
2021-10-31 14:40:30 +0000

    Added calculations Added logging for failed calculations

--- data.py
+++ data.py
@@ -1,10 +1,29 @@
 # Import data from local database
 from arctic import Arctic
+import logging
 
+logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
 store = Arctic('localhost')
-library = store['US.EOD']
 
-for symbol in library.list_symbols():
-    df = library.read(symbol)
-    print(df.data)
+read_library = store['US.EOD']
+write_library = store['US.EOD.CALC']
+
+for symbol in read_library.list_symbols():
+    # Load data from local DB
+    try:
+        df = read_library.read(symbol)
+        data = df.data
+
+        # Run calculations
+        data['intra_day_ret'] = (data['close'] / data['open']) - 1
+        data['total_day_ret'] = data['adjClose'].pct_change()
+        data['long_only_cum_ret'] = (1 + data.total_day_ret).cumprod() - 1
+
+        #Save to local write DB
+        write_library.write(symbol + "_CALC", data, metadata={'source': 'Financial Modelling Prep'})
+    except:
+            logging.error('%s raised an error', symbol)
+
+
+
