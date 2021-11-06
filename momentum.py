ed591f417ac09f88605009acfbba73f9e43d6c5d
1smay3 <spencermayy@gmail.com>
2021-11-06 22:00:29 +0000

    Added trigger signals for given moving averages

--- momentum.py
+++ momentum.py
@@ -2,6 +2,7 @@ from arctic import Arctic
 from config import *
 import matplotlib.pyplot as plt
 import logging
+import numpy as np
 
 logging.basicConfig(filename='momentum.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
@@ -12,10 +13,10 @@ write_library = store['US.EOD.MOM']
 test = ['AAPL_EOD_CALC']
 
 
-def momentum_signal(st, lt, row):
-    if row[st] < row[lt]:
+def momentum_signal(dataframe, short, long):
+    if dataframe[short] <= dataframe[long]:
         val = -1
-    elif row[st] > row[lt]:
+    elif dataframe[short] >= dataframe[long]:
         val = 1
     else:
         val = 0
@@ -28,13 +29,24 @@ repoed_testfile.data.to_excel("testfile.xlsx")
 for symbol in test:  # read_library.list_symbols():
     # Load data from local DB
     try:
+        # Read data for given symbol
         df = read_library.read(symbol)
         data = df.data
-        for key, value in signal_dict.items():
-            data[key] = data['adjClose'].rolling(window=value).mean().shift(1)
-            plot_data = data.drop(columns=['open', 'high', 'low', 'close', 'volume', 'unadjustedVolume', 'change',
-                                           'vwap', 'intra_day_ret', 'total_day_ret', 'long_only_cum_ret'])
+
     except:
         logging.error('%s raised an error', symbol)
+
+    # Calculate moving averages and label accordingly
+    for key, value in signal_dict.items():
+        data[key] = data['adjClose'].rolling(window=value).mean().shift(1)
+        plot_data = data.drop(columns=['open', 'high', 'low', 'close', 'volume', 'unadjustedVolume', 'change',
+                                       'vwap', 'intra_day_ret', 'total_day_ret', 'long_only_cum_ret'])
+
+    # Calculate signals given moving averages
+    for i in range(int(len(signal_dict) / 2)):
+        st = 'st_' + str((i + 1))
+        lt = 'lt_' + str((i + 1))
+        plot_data['ts_' + str((i + 1))] = plot_data.apply(momentum_signal, args=(str(st), lt), axis=1)
+
 plot_data.plot()
 plt.show()
